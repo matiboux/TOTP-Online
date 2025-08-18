07d4b41fc6d9fc7cc69a20bb2a421c71541efff3
Matiboux <matiboux@gmail.com>
2025-08-19 00:19:47 +0200

    Update index page

--- app/app/src/i18n/locales/fr.ts
+++ app/app/src/i18n/locales/fr.ts
@@ -3,7 +3,21 @@ import type { Diff } from '~/i18n/types.d.ts'
 import type { DefaultLocaleKeys } from './types.d.ts'
 
 const locale = {
-	'Welcome!': 'Bienvenue !',
+	// Index
+	'Online conversion tool for environment files.': 'Outil de conversion en ligne pour les fichiers d\'environnement.',
+	// Footer
+	'Open source project': 'Projet open source',
+	'See the source code on': 'Voir le code source sur',
+	'Built with': 'Construit avec',
+	'served by': 'servi par',
+	'Made with love by': 'Créé avec amour par',
+	'Data privacy': 'Confidentialité des données',
+	'No data is collected or processed over the network or on any server.':
+		'Aucune donnée n\'est collectée ou traitée sur le réseau ou sur un serveur.',
+	'All data is processed locally in your browser, and stays on your own device.':
+		'Toutes les données sont traitées localement dans votre navigateur et restent sur votre propre appareil.',
+	'This website uses no cookies and does no tracking.':
+		'Ce site web n\'utilise pas de cookies et ne fait pas de suivi.',
 } as const
 
 export default locale satisfies
