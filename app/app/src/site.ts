9c8cd8660d4cb04241f7b08ddde00c1744288e15
Matiboux <matiboux@gmail.com>
2025-08-19 00:12:24 +0200

    Add website files

---
+++ app/app/src/site.ts
@@ -0,0 +1,35 @@
+import { GITHUB_SHA, VERSION_TAG } from 'astro:env/client'
+
+import type { Props as BaseProps } from '~/layouts/Base.astro'
+
+export interface Site
+{
+	lang?: BaseProps['lang']
+	title?: BaseProps['title']
+	description?: BaseProps['description']
+	version?: BaseProps['version']
+	author?: BaseProps['author']
+	keywords?: BaseProps['keywords']
+	generator?: BaseProps['generator']
+	themeColor?: BaseProps['themeColor']
+	viewportScale?: BaseProps['viewportScale']
+	favicon?: BaseProps['favicon']
+	socialTitle?: BaseProps['socialTitle']
+	socialDescription?: BaseProps['socialDescription']
+	socialImage?: BaseProps['socialImage']
+	socialUrl?: BaseProps['socialUrl']
+	socialType?: BaseProps['socialType']
+	socialTwitterCard?: BaseProps['socialTwitterCard']
+}
+
+export const site: Site = {
+	lang: 'en',
+	title: 'TOTP Online',
+	description: 'Online generator for TOTP codes.',
+	version: GITHUB_SHA || VERSION_TAG || 'dev',
+	author: 'Matiboux',
+	themeColor: '#ffffff',
+	viewportScale: 1,
+	socialTitle: true,
+	socialDescription: true,
+}
